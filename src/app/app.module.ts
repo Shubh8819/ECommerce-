7eff4e50e2e70f7440500d3362c2f25d39e7bae5
Shubham Tiwari <shubhamtiwari@Shubhams-MacBook-Pro.local>
2024-01-18 10:58:32 +0530

    Component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponentComponent } from './component/product-list-component/product-list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponentComponent
   ],
   imports: [
     BrowserModule
